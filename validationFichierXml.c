7bddb9db004f7dfdc6d16ca736f0a91c524ec8d3
Sokhna98 <46223753+Sokhna98@users.noreply.github.com>
2019-01-09 23:49:29 +0000

    Add files via upload

---
+++ validationFichierXml.c
@@ -0,0 +1,15 @@
+96-#include <stdio.h>
+#include <libxml/parser.h>
+#include"validationFichierXml.h"
+int validationFichierXml (char const * monFichierXml)
+{
+	xmlDocPtr docxml;
+	docxml = xmlParseFile(monFichierXml);
+	if(docxml==NULL)
+	{
+		printf("Le fichier xml est invalide\n");
+		return 0;
+	}
+	xmlFreeDoc(docxml);
+	return 1;
+}
